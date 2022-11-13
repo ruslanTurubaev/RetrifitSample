a8b7900273c6e1480815062b03e0dc79a41f31cc
Ruslan Turubaev <turubaevruslan@gmail.com>
2022-11-13 18:46:58 +0300

    add comments to RetrofitServices.kt

--- app/src/main/java/com/example/retrofittester2/data/interfaces/RetrofitServices.kt
+++ app/src/main/java/com/example/retrofittester2/data/interfaces/RetrofitServices.kt
@@ -7,4 +7,11 @@ import retrofit2.http.GET
 interface RetrofitServices {
     @GET("marvel")
     fun getMovieList(): Call<MutableList<Movie>>
+
+    /*
+    *jfhsdjkhsdv
+    * dslfdlsnfdsh
+    * dsjfdjksbdjksb
+    * kjsdkjdsbkjds
+     */
 }
